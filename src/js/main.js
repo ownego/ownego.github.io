56b636ab6d2b67f71419a313175ab7d33160dc02
JuzSer <sonnh.90@gmail.com>
2015-06-15 16:05:46 +0700

    changing screen scripts added

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,28 @@
 (function(window, $){
     var $doc = $(document);
+    var $body = $('body');
 
     // Common functions
-//    var oe = {
-//        nextSwitch: function() {
-//
-//        },
-//
-//        nextActions
-//    };
-//
-//    // Make it global
-//    window.oe = oe;
+    var oe = {
+        nextSwitch: function() {
+            var screenCount = $('.main-screen').size();
+            $doc.on('click', '#nextBtn', function() {
+                var currentScreen = parseInt($body.attr('data-active-screen'));
+                var nextScreen = (currentScreen == screenCount) ? 1 : (currentScreen + 1);
+
+                $body.attr('data-active-screen', nextScreen);
+                $('[data-screen-no=' + currentScreen + ']').removeClass('active');
+                $('[data-screen-no=' + nextScreen + ']').addClass('active');
+            });
+        },
+
+        nextActions: function() {
+
+        }
+    };
+
+    // Make it global
+    window.oe = oe;
 })(window, jQuery);
 
 $(function() {
@@ -23,4 +34,6 @@ $(function() {
         $('.site-nav-wrapper').toggleClass('active');
         $('.first-half-page').toggleClass('active');
     });
+
+    oe.nextSwitch();
 });
