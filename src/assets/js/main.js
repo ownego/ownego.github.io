7ad072f6b3faf1d544c2487233b00f859a97f54e
JuzSer <sonnh.90@gmail.com>
2015-06-18 14:35:38 +0700

    letter effects added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,8 +16,10 @@
             });
         },
 
-        nextActions: function() {
-
+        effLogoName: function() {
+            $('.texttl').textillate({
+                initialDelay: 500
+            });
         }
     };
 
@@ -26,27 +28,37 @@
 })(window, jQuery);
 
 $(function() {
+    // Some elements must fit the screen height
+    //  so I create a specific class for all of 'em
     var screenHeight = $(window).outerHeight();
     $('.fitscreen').height(screenHeight);
 
+    // Navigation button
     $('.btn-nav').click(function() {
         $(this).children('.lines-button').toggleClass('close');
-        $('.site-nav-wrapper').toggleClass('active');
-        $('.first-half-page').toggleClass('active');
+        $('.site-nav-wrapper, .first-half-page').toggleClass('active');
     });
 
+    // Delay to show the effects
+    setTimeout(function() {
+        var firstScreen = 1;
+        $('body').attr('data-active-screen', firstScreen);
+        $('[data-screen-no=' + firstScreen + ']').addClass('active');
+    }, 500);
+
     oe.nextSwitch();
-    
-    // SVG
+    oe.effLogoName();
+
+    // Logo drawing via SVG
     var current_frame = 0;
     var total_frames = 120;
     var path = new Array();
     var length = new Array();
-    for(var i=0; i<3;i++){
+    for(var i = 0; i < 3; i++){
         path[i] = document.getElementById('i'+i);
         l = path[i].getTotalLength();
         length[i] = l;
-        path[i].style.strokeDasharray = l + ' ' + l; 
+        path[i].style.strokeDasharray = l + ' ' + l;
         path[i].style.strokeDashoffset = l;
     }
     var handle = 0;
