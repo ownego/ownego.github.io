5fe135d17db6506914483b5fc83c1c2af4ee3871
minhnt <minhnt@ownego.com>
2015-06-22 15:39:55 +0700

    Add oe console logo

--- src/assets/js/oe.main.js
+++ src/assets/js/oe.main.js
@@ -1,5 +1,3 @@
-console.log('======================== ownego.com ========================\n| Shhhh...\n| Hi! Are you finding something or you wanna join with us?\n| Maybe we are not big but... like you, we wanna know everything about the world.\n| \n| Hmm, and who know, maybe you are the one who we\'re finding to save the world together, huh?\n| Just contact us if you are feeling confuse right now, after you read this hidden copy.\n| --------------- \n| Via: sonnh[at]ownego.com.\n| You always be welcome :)\n============================================================');
-
 (function(window, $){
     var $doc = $(document);
     var $body = $('body');
