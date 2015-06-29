feac351e2ae7532aa4d3bda76c3e5e884ed9ec78
Minh Nguyen <leminh111@gmail.com>
2015-06-29 15:21:18 +0700

    form validation & forward to Slack

--- src/assets/js/oe.main.js
+++ src/assets/js/oe.main.js
@@ -88,4 +88,80 @@ $(function() {
 
     // functions call
     oe.switchActions();
+    
+    // Form Validation
+    $('#messageForm').validate({
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            message: "required"
+        },
+        
+        messages: {
+            name: {
+                required: "Caution*: Please enter your name"
+            },
+            email: {
+                required: "Caution*: Please enter your email",
+                email: "Caution*: Please enter a valid email"
+            },
+            message: "Caution*: Please enter your message"
+        },
+        errorPlacement: function(error, element) {
+            error.appendTo($('#errorContainer'));
+        },
+        // Focus the last active or first invalid element on submit
+        focusInvalid: true,
+        // If nothing is entered, all rules are skipped
+        onfocusout: true,
+        onkeyup: false,
+        
+        // Show only one error at a time
+        showErrors: function(errorMap, errorList) {
+            $('#messageForm').find('input, textarea').each(function() {
+                $(this).removeClass('error');
+            });
+            if(errorList.length) {
+                $('#errorContainer').html(errorList[0]['message']);
+                $(errorList[0]['element']).addClass('error');
+                $('#errorContainer').css('color', '#f00');
+            }
+        }, // end
+        
+        submitHandler: function(form) {
+             $('#modalSendForm').modal('show');
+        }
+    });
+    
+    $('#submitConfirm').click(function() {
+        if (!($('#g-recaptcha-response').val())) {
+            $('#submitAlert').html('Please check the captcha');
+            $('#submitAlert').css('color', '#f00');
+        } else {
+            // AJAX
+            $.ajax({
+                url: 'ajax.php',
+                type: 'post',
+                data: {
+                       'name': $('#name').val(),
+                       'email': $('#email').val(),
+                       'message': $('#message').val(),
+                       'g-recaptcha-response': $('#g-recaptcha-response').val()
+                },
+                success: function(res) {
+                    $('#cautionWrapper').html(res);
+                    $('#modalSendForm').modal('hide');
+                },
+                error: function(xhr, desc, err) {
+                    $('#errorContainer').html("Caution*: There are some problems sending your message. Please try again later.");
+                    $('#errorContainer').css('color', '#f00');
+                    console.log(xhr);
+                    console.log("Details: " + desc + "\nError:" + err);
+                }
+            }); // end ajax call
+        }
+    })
 });
